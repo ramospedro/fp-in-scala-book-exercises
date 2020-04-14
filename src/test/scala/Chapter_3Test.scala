b295addf0a9f379eb9f12d586219b0f927f58536
Pedro Ramos <ppedro.ramoss@gmail.com>
2020-04-14 20:02:36 -0300

    Add missing whitespace

--- src/test/scala/Chapter_3Test.scala
+++ src/test/scala/Chapter_3Test.scala
@@ -4,7 +4,7 @@ import Chapter_3._
 class Chapter_3Test extends FlatSpec with Matchers {
   "list" should "put the items in the exact order as they are passed" in {
     val list = SingleLinkedList.List(10, 20, 30, 40, 50)
-    val stringfiedList =SingleLinkedList.List.stringfy(list)
+    val stringfiedList = SingleLinkedList.List.stringfy(list)
     
     stringfiedList should be ("10, 20, 30, 40, 50");
   }
