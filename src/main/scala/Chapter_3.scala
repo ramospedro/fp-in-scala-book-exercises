3b9fcbe286035eba0a115fe548d9b83f493cb80b
Pedro Ramos <ppedro.ramoss@gmail.com>
2020-04-14 22:09:26 -0300

    Add more functions for handling list operations

--- src/main/scala/Chapter_3.scala
+++ src/main/scala/Chapter_3.scala
@@ -5,15 +5,40 @@ object Chapter_3 {
     case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
     object List {
+      def sum(ints: List[Int]): Int = ints match {
+        case Nil => 0
+        case Cons(x,xs) => x + sum(xs)
+      }
+
+      def product(ds: List[Double]): Double = ds match {
+        case Nil => 1.0
+        case Cons(0.0, _) => 0.0
+        case Cons(x,xs) => x * product(xs)
+      }
+
       def stringfy[A](as: List[A]): String = as match {
         case Nil => ""
         case Cons(x, Nil) => x.toString
         case Cons(x, xs) => x.toString + ", " + stringfy(xs)
       }
 
+      def tail[A](as: List[A]): List[A] = as match {
+        case Nil => Nil
+        case Cons(_, xs) => xs
+      }
+
       def apply[A](as: A*): List[A] =
         if (as.isEmpty) Nil
         else Cons(as.head, apply(as.tail: _*))
     }
+
+    // val x = List(1,2,3,4,5) match {
+    //   case Cons(x, Cons(2, Cons(4, _))) => x
+    //   case Nil => 42
+    //   case Cons(x, Cons(y, Cons(3, Cons(4, _)))) =>x+y
+    //   case Cons(h, t) => h + sum(t)
+    //   case _ => 101
+    // }
+    // result is 3, matching the third expression
   }
 }
--- src/test/scala/Chapter_3Test.scala
+++ src/test/scala/Chapter_3Test.scala
@@ -1,11 +1,23 @@
 import org.scalatest._
 import Chapter_3._
+import Chapter_3.SingleLinkedList.List._
 
 class Chapter_3Test extends FlatSpec with Matchers {
   "list" should "put the items in the exact order as they are passed" in {
     val list = SingleLinkedList.List(10, 20, 30, 40, 50)
-    val stringfiedList = SingleLinkedList.List.stringfy(list)
-    
-    stringfiedList should be ("10, 20, 30, 40, 50");
+    stringfy(list) should be ("10, 20, 30, 40, 50");
+  }
+
+  "tail" should "remove the first element from the list" in {
+    val list = SingleLinkedList.List(10, 20, 30, 40, 50)
+    tail(list) should be (SingleLinkedList.List(20, 30, 40, 50))
+  }
+
+  "tail" should "return an empty list if the list is already empty" in {
+    tail(SingleLinkedList.Nil) should be (SingleLinkedList.Nil)
+  }
+
+  "tail" should "return an empty list when the list has only one element" in {
+    tail(SingleLinkedList.List(10)) should be (SingleLinkedList.Nil)
   }
 }
