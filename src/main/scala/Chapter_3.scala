c52069066f04e4d2ac419f9a0f6ee8deb9f8eae9
Pedro Ramos <ppedro.ramoss@gmail.com>
2020-04-15 10:23:55 -0300

    Change method order

--- src/main/scala/Chapter_3.scala
+++ src/main/scala/Chapter_3.scala
@@ -5,6 +5,10 @@ object Chapter_3 {
     case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
     object List {
+      def apply[A](as: A*): List[A] =
+        if (as.isEmpty) Nil
+        else Cons(as.head, apply(as.tail: _*))
+        
       def sum(ints: List[Int]): Int = ints match {
         case Nil => 0
         case Cons(x,xs) => x + sum(xs)
@@ -32,9 +36,7 @@ object Chapter_3 {
         case Cons(_, xs) => Cons(a, xs)
       }
 
-      def apply[A](as: A*): List[A] =
-        if (as.isEmpty) Nil
-        else Cons(as.head, apply(as.tail: _*))
+
     }
 
     // val x = List(1,2,3,4,5) match {
