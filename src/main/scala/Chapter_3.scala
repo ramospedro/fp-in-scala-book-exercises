07501f0cfac06ee8744139cc5ab7ba51905282dd
Pedro Ramos <ppedro.ramoss@gmail.com>
2020-04-15 23:42:12 -0300

    Add more list operations

--- src/main/scala/Chapter_3.scala
+++ src/main/scala/Chapter_3.scala
@@ -27,16 +27,63 @@ object Chapter_3 {
       }
 
       def tail[A](as: List[A]): List[A] = as match {
-        case Nil => Nil
+        case Nil => sys.error("tail on empty list")
         case Cons(_, xs) => xs
       }
 
       def setHead[A](as: List[A], a: A): List[A] = as match {
-        case Nil => Nil
+        case Nil => sys.error("setHead on empty list")
         case Cons(_, xs) => Cons(a, xs)
       }
 
+      def drop[A](l: List[A], n: Int): List[A] =
+        if (n <= 0) l
+        else l match {
+          case Nil => Nil
+          case Cons(_, xs) => drop(xs, n - 1)
+        }
 
+      def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+        case Cons(h, t) if f(h) => dropWhile(t, f)
+        case _ => l
+      }
+
+      def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+        case Nil => a2
+        case Cons(h, t) => Cons(h, append(t, a2))
+      }
+
+      def init[A](l: List[A]): List[A] = l match {
+        case Nil => sys.error("init in empty list")
+        case Cons(_, Nil) => Nil
+        case Cons(h, t) => Cons(h, init(t))
+      }
+
+      def init2[A](l: List[A]): List[A] = {
+        val buf = new collection.mutable.ListBuffer[A]
+        @annotation.tailrec
+        def go(cur: List[A]): List[A] = cur match {
+          case Nil => sys.error("init in empty list")
+          case Cons(_, Nil) => List(buf.toList: _*)
+          case Cons(h, t) => buf += h; go(t)
+        }
+        go(l)
+      }
+
+      def init3[A](l: List[A]): List[A] = {
+        reverse(tail(reverse(l)))
+      }
+
+      def reverse[A](l: List[A]): List[A] = {
+        @annotation.tailrec
+        def go(cur: List[A], acc: List[A]): List[A] = cur match {
+          case Nil => sys.error("reverse in empty list")
+          case Cons(h, Nil) => Cons(h, acc)
+          case Cons(h, t) => go(t, Cons(h, acc))
+        }
+
+        go(l, Nil)
+      }
     }
 
     // val x = List(1,2,3,4,5) match {
@@ -46,6 +93,6 @@ object Chapter_3 {
     //   case Cons(h, t) => h + sum(t)
     //   case _ => 101
     // }
-    // result is 3, matching the third expression
+
   }
 }
--- src/test/scala/Chapter_3Test.scala
+++ src/test/scala/Chapter_3Test.scala
@@ -14,7 +14,7 @@ class Chapter_3Test extends FlatSpec with Matchers {
   }
 
   "tail" should "return an empty list if the list is already empty" in {
-    tail(SingleLinkedList.Nil) should be (SingleLinkedList.Nil)
+    an [Exception] should be thrownBy (tail(SingleLinkedList.Nil))
   }
 
   "tail" should "return an empty list when the list has only one element" in {
@@ -25,7 +25,47 @@ class Chapter_3Test extends FlatSpec with Matchers {
     setHead(SingleLinkedList.List(10, 20, 30), 0) should be (SingleLinkedList.List(0, 20, 30))
   }
 
-  "setHead" should "return an empty list if the list is already empty" in {
-    setHead(SingleLinkedList.Nil, 0) should be (SingleLinkedList.Nil)
+  "setHead" should "throw and exception if the list is empty" in {
+    an [Exception] should be thrownBy (setHead(SingleLinkedList.Nil, 0))
+  }
+
+  "drop" should "remove the first n elements from a list" in {
+    drop(SingleLinkedList.List(10, 20, 30, 40, 50, 60, 70, 80), 4) should be (SingleLinkedList.List(50, 60, 70, 80))
+  }
+
+  "drop" should "return an empty list if the list was already empty" in {
+    drop(SingleLinkedList.Nil, 2) should be (SingleLinkedList.Nil)
+  }
+
+  "drop" should "return an empty list if n is equal or greater than the list length" in {
+    drop(SingleLinkedList.List(10, 20, 30), 3) should be (SingleLinkedList.Nil)
+    drop(SingleLinkedList.List(10, 20), 3) should be (SingleLinkedList.Nil)
+  }
+
+  "dropWhile" should "drop items from the list as long as they match the the predicate" in {
+    dropWhile(SingleLinkedList.List(10, 20, 30, 40, 50), (x: Int) => x < 30) should be (SingleLinkedList.List(30, 40, 50))
+  }
+
+  "append" should "append second list in the end of the first list" in {
+    append(
+      SingleLinkedList.List(10, 20, 30),
+      SingleLinkedList.List(40, 50, 60)
+    ) should be (SingleLinkedList.List(10, 20, 30, 40, 50, 60))
+  }
+
+  "init" should "return all but the last element from a list" in {
+    init(SingleLinkedList.List(10, 20, 30, 40, 50)) should be (SingleLinkedList.List(10, 20, 30, 40))
+  }
+
+  "init" should "throw and exception if the list is empty" in {
+    an [Exception] should be thrownBy (init(SingleLinkedList.Nil))
+  }
+
+  "init2" should "return all but the last element from a list" in {
+    init2(SingleLinkedList.List(10, 20, 30, 40, 50)) should be (SingleLinkedList.List(10, 20, 30, 40))
+  }
+
+  "init3" should "return all but the last element from a list" in {
+    init3(SingleLinkedList.List(10, 20, 30, 40, 50)) should be (SingleLinkedList.List(10, 20, 30, 40))
   }
 }
